0dc4aa360e5a8552b2415ee70aaa36d7131c432c
Juan ismael Camacho cervantes <2000084883@hexaware.com>
2022-10-13 09:58:15 -0500

    Agregado el caso 2 con testng.

--- src/main/java/hexaware/seleniumcourse/App.java
+++ src/main/java/hexaware/seleniumcourse/App.java
@@ -12,22 +12,26 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class App {
-
-	@BeforeTest
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeMethod
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 	}
 	
 	@Test 
 	public void test1(){
-		WebDriver driver = new ChromeDriver();
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		driver.manage().window().maximize();
 		// STEP 1
 		driver.get("http://automationpractice.com/index.php");
@@ -38,7 +42,7 @@ public class App {
 		// STEP 3
 		WebElement emailCreateTextBox = driver.findElement(By.id("email_create"));
 		wait.until(ExpectedConditions.visibilityOf(emailCreateTextBox));
-		emailCreateTextBox.sendKeys("email1234@yopmail.com");
+		emailCreateTextBox.sendKeys("email0009@yopmail.com");
 		// STEP 4
 		WebElement submitInformation = driver.findElement(By.id("SubmitCreate"));
 		wait.until(ExpectedConditions.visibilityOf(submitInformation));
@@ -78,8 +82,36 @@ public class App {
 		Assert.assertTrue(driver.findElement(By.id("my-account")).isDisplayed());
 	}
 	
-	@AfterClass
-	public void exit(WebDriver driver) {
-		driver.close();
+	@Test
+	public void test2() {
+		// SECOND PART
+		driver.manage().window().maximize();
+		// STEP 1
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// STEP 2
+		WebElement signInButton = driver.findElement(By.className("login"));
+		wait.until(ExpectedConditions.elementToBeClickable(signInButton));
+		signInButton.click();
+		// STEP 3
+		WebElement emailAddressSignIn = driver.findElement(By.id("email"));
+		emailAddressSignIn.sendKeys("email@@yopmail.com");
+		driver.findElement(By.id("SubmitLogin")).click();
+		// STEP 4
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement errorWarning = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div[1]/ol/li"));
+		Assert.assertEquals(errorWarning.getText(), "Invalid email address.");
+	}
+	
+	@AfterMethod
+	public void exit() {
+		System.out.println("Cerrando navegador.");
+		try {
+			driver.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
 	}
 }
